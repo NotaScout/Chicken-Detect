05b63394e4882d7255ef6760e40cbcd2b1545fb5
NotaScout <154877329+NotaScout@users.noreply.github.com>
2025-06-08 21:00:46 -0500

    alternate thoughts on structure (nonreset)

--- Autodoor.py
+++ Autodoor.py
@@ -1,7 +1,43 @@
 # defines autodoor object
+# generic # includes
+import os
+
+# Omlet SDK
+from dotenv import load_dotenv
+from smartcoop.client import SmartCoopClient 
+from smartcoop.api.omlet import Omlet 
+
+# device objects
+from Autodoor import Autodoor
 
 
+#  Load the .env file FIRST
+load_dotenv("ENVIRON.env")
 
+#  Then read from it
+api_key_d1 = os.getenv("DOOR_1_API_KEY")
+db_name_d1 = os.getenv("DB_NAME")
+debug_mode = os.getenv("DEBUG")
+
+#  Then use it
+client = SmartCoopClient(client_secret=api_key_d1)
+omlet = Omlet(client)
+deviceInfo = omlet.get_devices()
+dev0 = deviceInfo[0]
+# Print values
+
+
+# make in future grab specific named door
+def fetchDoor():
+    
+    return dev0
+
+
+class OMLET_API():
+    def __init__(self):
+        self.APIKEY = api_key_d1
+        self.DBINFO = db_name_d1
+        self.DBMODE = debug_mode
 
 class Autodoor():
     def __init__(self, doorInfo):
@@ -12,12 +48,19 @@ class Autodoor():
         self.state = doorInfo.state
         self.configuration = doorInfo.configuration
         self.actions = doorInfo.actions
-
     '''
     6/8/25
 
     opens autodoor
     '''
     async def open_door():
-        pass
+        omlet
     
+
+
+'''
+print(f"API Key: {api_key_d1}")
+print(f"Database: {db_name_d1}")
+print(f"Debug mode: {debug_mode}")
+print(f"Devices={dev0}")
+'''
